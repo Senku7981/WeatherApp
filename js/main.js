c416cff0ab1a955f7baa32c738fffc92bdbfe556
Senku7981 <at437947@gmail.com>
2023-07-28 20:26:43 +0500

    Добавил создание и вывод карточки через Js

--- js/main.js
+++ js/main.js
@@ -5,24 +5,26 @@ const APIKey = 'e8099645bce04f81827133212232807'
 
 
 
-// Получаем название города 
-
+// Элементы на странице
+const header = document.querySelector('#header');
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
-let city;
-// Слушаем отправку формы
 
+
+
+// Слушаем отправку формы
 form.onsubmit = function(e) {
     //  Отменяем отправку формы
     e.preventDefault();
 
     // Берем значения из Input, обрезаем пробелы и табы
 
-    city = input.value.trim();
+    let city = input.value.trim();
 
+    // Адрес запроса 
     const url  = `http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}`;
 
-
+    // Выполнение запроса 
     fetch(url)
         .then((response) => {
             return response.json();
@@ -30,6 +32,30 @@ form.onsubmit = function(e) {
     
     .then((data) => {
         console.log(data);
+        console.log(data.location.name);
+        console.log(data.location.country);
+        console.log(data.current.temp_c);
+        console.log(data.current.condition.text);
+
+
+        // Отображаем на странице
+        // Разметка для карточки  
+    const html = `
+         <div class="card">
+            <h2 class="card-city">${data.location.name}<span>${data.location.country}</span></h2>
+            <div class="card-weather">
+                <div class="card-value">${data.current.temp_c}<sup>°c</sup></div>
+                <img class="card-img" src="img/CloudRain.png" alt="weather icon">
+            </div>
+        <div class="card-description">
+            ${data.current.condition.text}
+        </div>
+    </div>
+    `;
+
+    // Отображаем карточку на странице
+
+        header.insertAdjacentHTML('afterend', html);
     });
 
 }
