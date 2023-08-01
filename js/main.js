f07622ad7750c5ed95528d1a146eb6de2827f23d
Senku7981 <at437947@gmail.com>
2023-08-01 19:37:00 +0500

    Добавил вывод иконок и русский язык

--- js/main.js
+++ js/main.js
@@ -1,80 +1,93 @@
-const APIKey = 'e8099645bce04f81827133212232807'
+import conditions from './conditions.js';
 
+console.log(conditions);
 
-// http://api.weatherapi.com/v1/current.json?key=e8099645bce04f81827133212232807&q=London  пример запроса
-
+const apiKey = 'e9a5d3b74bf84418b11193028231901';
 
 // Элементы на странице
-const header = document.querySelector('#header');
+const header = document.querySelector('.header');
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
 
-const removeCard = () => {
-    const oldCard = document.querySelector('.card')
-    if (oldCard) oldCard.remove();
+function removeCard() {
+	const prevCard = document.querySelector('.card');
+	if (prevCard) prevCard.remove();
 }
 
-const showError = (errorMessage) => {
-    const html = `<div class="card">${errorMessage}</div>`
-    header.insertAdjacentHTML('afterend', html);
+function showError(errorMessage) {
+	// Отобразить карточку с ошибкой
+	const html = `<div class="card">${errorMessage}</div>`;
+
+	// Отображаем карточку на странице
+	header.insertAdjacentHTML('afterend', html);
 }
 
+function showCard({ name, country, temp, condition, imgPath }) {
+	// Разметка для карточки
+	const html = `<div class="card">
+                                <h2 class="card-city">${name} <span>${country}</span></h2>
 
-const showCard = ({name, country, temp, condition}) => {
-    const html = `
-        <div class="card">
-                <h2 class="card-city">${name}<span>${country}</span></h2>
-                <div class="card-weather">
-                    <div class="card-value">${temp}<sup>°c</sup></div>
-                    <img class="card-img" src="img/CloudRain.png" alt="weather icon">
-                </div>
-            <div class="card-description">
-                ${condition}
-            </div>
-        </div>
-        `;
+                                <div class="card-weather">
+                                    <div class="card-value">${temp}<sup>°c</sup></div>
+                                    <img class="card-img" src="${imgPath}" alt="Weather">
+                                </div>
 
-    header.insertAdjacentHTML('afterend', html);
+                                <div class="card-description">${condition}</div>
+                            </div>`;
+
+	// Отображаем карточку на странице
+	header.insertAdjacentHTML('afterend', html);
 }
 
 async function getWeather(city) {
-    const url = `http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+	const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log(data);
     return data;
 }
 
-
-
 // Слушаем отправку формы
 form.onsubmit = async function (e) {
-    //  Отменяем отправку формы
-    e.preventDefault();
+	// Отменяем отправку формы
+	e.preventDefault();
 
-    // Берем значения из Input, обрезаем пробелы
-    let city = input.value.trim();
+	// Берем значение из инпута, обрезаем пробелы
+	let city = input.value.trim();
 
     // Получаем данные с сервера
     const data = await getWeather(city);
 
+    if (data.error) {
+		removeCard();
+		showError(data.error.message);
+	} else {
+		removeCard();
 
-    if(data.error) {
-        removeCard();
-        showError(data.error.message);
-    } else {
-        removeCard();
+		console.log(data.current.condition.code);
 
+		const info = conditions.find(
+			(obj) => obj.code === data.current.condition.code
+		);
+		console.log(info);
+		console.log(info.languages[23]['day_text']);
 
-        const weatherData = {
-            name: data.location.name,
-            country: data.location.country,
-            temp: data.current.temp_c,
-            condition: data.current.condition.text
-        }
+        const filePath = './img/' + (data.current.is_day ? 'day' : 'night') + '/';
+		const fileName = (data.current.is_day ? info.day : info.night) + '.png';
+        const imgPath = filePath + fileName;
+        console.log('filePath', filePath + fileName);
 
-        showCard(weatherData);
-    }
+		const weatherData = {
+			name: data.location.name,
+			country: data.location.country,
+			temp: data.current.temp_c,
+			condition: data.current.is_day
+				? info.languages[23]['day_text']
+				: info.languages[23]['night_text'],
+			imgPath,
+		};
 
+		showCard(weatherData);
+	}
 
-}
+};
